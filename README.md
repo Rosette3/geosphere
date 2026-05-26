901203195c6d2d8f6a7889fbd7518a5b274a57f5
Rosette3 <tanarootintirat@gmail.com>
2026-05-26 18:40:56 +0700

    The changes are just starters of the html and js for id for the news articles.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # geosphere
 Interactive 3D globe visualization project using Three.js
+
+Log 1 - Created github and html js and css files, Created the basic web of html and script for example news to try on.
--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 
+let news = [
+    {
+    title: "Typhoon Near Okinawa",
+    country: "Japan",
+    desc: "Heavy rain and evacuation warnings issued."
+    },
+
+    {
+    title: "New Railway Project Approved",
+    country: "Thailand",
+    desc: "Government announces major transport expansion."
+    },
+
+    {
+    title: "Parrot Escapes Local Zoo",
+    country: "Brazil",
+    desc: "Bird reportedly learned to imitate alarms."
+    },
+
+    {
+    title: "AI Traffic System Tested",
+    country: "South Korea",
+    desc: "Smart traffic management enters trial phase."
+    }
+
+]
