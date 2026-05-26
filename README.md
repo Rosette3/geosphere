c1288307b3c284cdbb628436df5161c95d420931
Rosette3 <tanarootintirat@gmail.com>
2026-05-26 18:42:16 +0700

    a

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Interactive 3D globe visualization project using Three.js
 
 Log 1 - Created github and html js and css files, Created the basic web of html and script for example news to try on.
+Log 2 -
--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 
 let news = [
     {
+    id: 1,
     title: "Typhoon Near Okinawa",
     country: "Japan",
     desc: "Heavy rain and evacuation warnings issued."
     },
 
     {
+    id: 2,
     title: "New Railway Project Approved",
     country: "Thailand",
     desc: "Government announces major transport expansion."
     },
 
     {
+    id: 3,
     title: "Parrot Escapes Local Zoo",
     country: "Brazil",
     desc: "Bird reportedly learned to imitate alarms."
     },
 
     {
+    id: 4,
     title: "AI Traffic System Tested",
     country: "South Korea",
     desc: "Smart traffic management enters trial phase."
