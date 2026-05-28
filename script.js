ce44bf36dfaac0141fd5bdd13cd987438b01e5e6
Rosette3 <tanarootintirat@gmail.com>
2026-05-28 21:34:38 +0700

    yes.

--- script.js
+++ script.js
@@ -2,16 +2,17 @@
 let news = [
     {
     id: 1,
-    title: "Typhoon Near Okinawa",
+    title: "Tokyo Tests AI Train Monitoring System",
     country: "Japan",
     image_set: "Flag_of_Japan.svg.png",
-    desc: "Heavy rain and evacuation warnings issued."
+    desc: "Rail operators across Tokyo have begun testing a new AI-assisted monitoring system designed to improve railway safety, predict maintenance issues, and reduce train delays during peak commuting hours. The system analyzes real-time camera feeds and operational data to detect unusual activity, overcrowding, and potential technical failures before they escalate into larger disruptions. Officials stated the project is part of Japan’s broader push toward smart infrastructure modernization ahead of future transportation expansion plans."
     },
 
     {
     id: 2,
     title: "New Railway Project Approved",
     country: "Thailand",
+    image_set: "Flag_of_Thailand.svg.png",
     desc: "Government announces major transport expansion."
     },
 
@@ -19,6 +20,7 @@ let news = [
     id: 3,
     title: "Parrot Escapes Local Zoo",
     country: "Brazil",
+    image_set: "Flag_of_Brazil.svg.png",
     desc: "Bird reportedly learned to imitate alarms."
     },
 
@@ -26,6 +28,7 @@ let news = [
     id: 4,
     title: "AI Traffic System Tested",
     country: "South Korea",
+    image_set: "Flag_of_South_Korea.svg.png",
     desc: "Smart traffic management enters trial phase."
     }
 
@@ -52,9 +55,9 @@ function GenerateNews() {
     //News information randomized ! ! !
     container.innerHTML = `
     <h>${selected.title}</h>
-    <img src="Flags/${selected.image_set}">
-    <p>${selected.country}</p>
-    <p>${selected.desc}</p>
+    <div class="country-row"><sac>${selected.country}</sac><img class="Flags"
+    src="Flags/${selected.image_set}"></div>
+    <desc1>${selected.desc}</desc1>
     `;
     console.log(randomindexgen);
 }
